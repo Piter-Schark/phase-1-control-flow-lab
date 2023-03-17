2ffc166c452090dc74658893c032c9a92defb15b
Peter <pschoenbauer@gmail.com>
2023-03-16 20:50:50 -0400

    control flow lab with terif else if ternary and switch

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result
+  if (someValue <= 400){
+    result = 'This one is on me!'
+  }
+  else if (someValue <= 2000 ){
+    result = 'That will be twenty bucks.'
+  }
+  else if (someValue <= 2500 ){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500 ){
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+let  city = someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+return city
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  let tip;
+  switch (someTip) {
+    case 'generous':
+      tip = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      tip = 'Thank you.'
+      break;
+  
+    default:
+      tip = 'Bye.'
+      break;
+  }
+  return tip
 }
